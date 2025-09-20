acc5c18fa4c2f5c2406b171992a22325c7646c1b
JEEVESH-PG <97247708+jeevesh2162@users.noreply.github.com>
2025-09-21 04:30:30 +0530

    Add files via upload

--- Q17a.c
+++ Q17a.c
@@ -1,11 +1,10 @@
 /*
 ============================================================================
-Name : Q17a.c
+Name : Q17b.c
 Author : Jeevesh Rai
-Description : Write a program to simulate online ticket reservation. Implement write lock
-Write a program to open a file, store a ticket number and exit. Write a separate program, to
-open the file, implement write lock, read the ticket number, increment the number and print
-the new ticket number then close the file.
+Description : Online ticket reservation with write lock.
+              Open the file, lock it, read ticket number, increment,
+              print new number, update the file, unlock, and exit.
 Date: 28th Aug, 2025.
 ============================================================================
 */
@@ -19,28 +18,56 @@ struct ticket {
     int ticket_num;
 };
 
-int main(int argc, char *buff[])
-{
-    int a = open(buff[1], O_RDWR | O_CREAT, 0644);
-    if (a == -1)
-    {
-        perror("Error in opening file!");
-        return 0;
+int main(int argc, char *argv[]) {
+    int fd = open(argv[1], O_RDWR);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1;
     }
 
-    struct ticket t; 
-    int num;
-    printf("Enter ticket number:\t");
-    scanf("%d", &num);
+    struct flock lock;
+    lock.l_type = F_WRLCK;   // Write lock
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;          // Lock whole file
+    lock.l_pid = getpid();
 
-    t.ticket_num = num;
-    write(a, &t, sizeof(t));
+    printf("Trying to acquire lock...\n");
+    if (fcntl(fd, F_SETLKW, &lock) == -1) {
+        perror("Locking failed");
+        return 1;
+    }
+    printf("Lock acquired by PID %d\n", getpid());
 
-    printf("ticket number is stored in file\n");
+    // Read current ticket
+    struct ticket t;
+    lseek(fd, 0, SEEK_SET);
+    read(fd, &t, sizeof(t));
+
+    printf("Current ticket: %d\n", t.ticket_num);
+
+    // Increment ticket
+    t.ticket_num++;
+
+    // Move back and write updated value
+    lseek(fd, 0, SEEK_SET);
+    write(fd, &t, sizeof(t));
+
+    printf("New ticket: %d\n", t.ticket_num);
+
+    // Unlock file
+    lock.l_type = F_UNLCK;
+    if (fcntl(fd, F_SETLK, &lock) == -1) {
+        perror("Unlocking failed");
+    }
+    printf("Lock released\n");
+
+    close(fd);
     return 0;
 }
-/*
-Output:
-Enter ticket number:    10
-ticket number is stored in file
-*/
+
+// Trying to acquire lock...
+// Lock acquired by PID 1873
+// Current ticket: 0
+// New ticket: 1
+// Lock released
